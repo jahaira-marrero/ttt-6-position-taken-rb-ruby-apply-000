4d5a1a1f6324938d3167662f591187d5d4a54fa5
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-07 04:01:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,position)
   taken = nil 
-  if board[position] == '" "' || '""'
+  if board[position] == " " || ""
     taken = false 
   else board[position] == "X" || "O"
     taken = true 
