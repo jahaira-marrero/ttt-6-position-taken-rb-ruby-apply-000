687fd05c5762da4cabd95c44e3260c265f8b9560
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-07 03:36:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,8 @@ def position_taken?(board,position)
   taken = nil 
   if board[position] == '" "' || '""'
     taken = false 
+  elsif board[position] == "X" || "O"
+    taken = true
   else
     taken = true 
   end
